83879f7ac0e874f5f968f6a6b45dbb71b54cb129
Stefan Schmidt <stemschmidt@gmail.com>
2022-12-26 21:30:58 +0100

    initial commit

---
+++ application/src/main.c
@@ -0,0 +1,44 @@
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+#include <drivers/sensor.h>
+#include <drivers/eeprom.h>
+#include <stdio.h>
+#include <logging/log.h>
+
+#include <stdio.h>
+
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+LOG_MODULE_REGISTER(main);
+/* The devicetree node identifier for the "led0" alias. */
+
+void main(void)
+{
+	int ret;
+
+	if (!device_is_ready(led.port)) {
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return;
+		}
+		LOG_INF(".");
+		k_msleep(1000);
+	}
+}
